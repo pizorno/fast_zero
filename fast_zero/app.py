32aaa334ab3b1c36ec8e8ab907c0734bfad7bac5
Eduardo Pizorno <pizorno@gmail.com>
2024-11-30 18:12:14 -0300

    :sparkles: feat: Adicionando rotas create_user, read_users e seus testes

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -2,11 +2,19 @@ from http import HTTPStatus
 
 from fastapi import FastAPI
 
-from fast_zero.shcemas import Message
+from fast_zero.shcemas import UserDB, UserList, UserPublic, UserSchema
 
 app = FastAPI()
+database = []
 
 
-@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
-def read_root():
-    return {'message': 'Olá Mundo!'}
+@app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserSchema):
+    user_with_id = UserDB(id=len(database) + 1, **user.model_dump())
+    database.append(user_with_id)
+    return user_with_id
+
+
+@app.get('/users/', status_code=HTTPStatus.OK, response_model=UserList)
+def read_users():
+    return {'users': database}
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,12 +1,32 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
 
-from fast_zero.app import app
+def test_create_user(client):
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'testusername',
+            'password': 'password',
+            'email': 'test@email.com',
+        },
+    )
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        'id': 1,
+        'username': 'testusername',
+        'email': 'test@email.com',
+    }
 
 
-def test_read_root():
-    client = TestClient(app)
-    response = client.get('/')
-    assert response.status_code == HTTPStatus.OK
-    assert response.json() == {'message': 'Olá Mundo!'}
+def test_read_users(client):
+    reponse = client.get('/users')
+    assert reponse.status_code == HTTPStatus.OK
+    assert reponse.json() == {
+        'users': [
+            {
+                'id': 1,
+                'username': 'testusername',
+                'email': 'test@email.com',
+            }
+        ]
+    }
