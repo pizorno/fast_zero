aa3d473b3e3ea7032790efea8a622e244ab4d64f
Eduardo Pizorno <pizorno@gmail.com>
2024-12-01 09:16:44 -0300

    :sparkles: feat: Adicionando rotas delete, put e testes

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,8 @@
 from http import HTTPStatus
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
-from fast_zero.shcemas import UserDB, UserList, UserPublic, UserSchema
+from fast_zero.shcemas import Message, UserDB, UserList, UserPublic, UserSchema
 
 app = FastAPI()
 database = []
@@ -18,3 +18,32 @@ def create_user(user: UserSchema):
 @app.get('/users/', status_code=HTTPStatus.OK, response_model=UserList)
 def read_users():
     return {'users': database}
+
+
+@app.put(
+    '/users/{user_id}',
+    status_code=HTTPStatus.OK,
+    response_model=UserPublic,
+)
+def update_user(user_id: int, user: UserSchema):
+    if user_id < 1 or user_id > len(database):
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
+        )
+    user_with_id = UserDB(**user.model_dump(), id=user_id)
+    database[user_id - 1] = user_with_id
+    return user_with_id
+
+
+@app.delete(
+    '/users/{user_id}',
+    status_code=HTTPStatus.OK,
+    response_model=Message,
+)
+def delete_user(user_id: int):
+    if user_id < 1 or user_id > len(database):
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
+        )
+    del database[user_id - 1]
+    return {'message': 'User deleted!'}
--- tests/test_app.py
+++ tests/test_app.py
@@ -30,3 +30,25 @@ def test_read_users(client):
             }
         ]
     }
+
+
+def test_update_user(client):
+    response = client.put(
+        '/users/1',
+        json={
+            'username': 'teste2',
+            'password': '123123',
+            'email': 'test@email.com',
+            'id': 1,
+        },
+    )
+    assert response.json() == {
+        'username': 'teste2',
+        'email': 'test@email.com',
+        'id': 1,
+    }
+
+
+def test_delete_user(client):
+    response = client.delete('/users/1')
+    assert response.json() == {'message': 'User deleted!'}
