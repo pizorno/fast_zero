e36659341ee03fc72831754ee7e90453a61b6c2b
Eduardo Pizorno <pizorno@gmail.com>
2024-12-06 14:58:16 -0300

    :sparkles: feat: Adicionando rota create_user com banco de dados

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,7 +1,11 @@
 from http import HTTPStatus
 
 from fastapi import FastAPI, HTTPException
+from sqlalchemy import create_engine, select
+from sqlalchemy.orm import Session
 
+from fast_zero.models import User
+from fast_zero.settings import Settings
 from fast_zero.shcemas import Message, UserDB, UserList, UserPublic, UserSchema
 
 app = FastAPI()
@@ -10,9 +14,31 @@ database = []
 
 @app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
 def create_user(user: UserSchema):
-    user_with_id = UserDB(id=len(database) + 1, **user.model_dump())
-    database.append(user_with_id)
-    return user_with_id
+    engine = create_engine(Settings().DATABASE_URL)
+    with Session(engine) as session:
+        db_user = session.scalar(
+            select(User).where(
+                (User.username == user.username) | (User.email == user.email)
+            )
+        )
+        if db_user:
+            if db_user.username == user.username:
+                raise HTTPException(
+                    status_code=HTTPStatus.BAD_REQUEST,
+                    detail='Username already exists',
+                )
+            elif db_user.email == user.email:
+                raise HTTPException(
+                    status_code=HTTPStatus.BAD_REQUEST,
+                    detail='Email already exists',
+                )
+        db_user = User(
+            username=user.username, password=user.password, email=user.email
+        )
+        session.add(db_user)
+        session.commit()
+        session.refresh(db_user)
+        return db_user
 
 
 @app.get('/users/', status_code=HTTPStatus.OK, response_model=UserList)
